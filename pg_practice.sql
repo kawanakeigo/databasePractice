dbf5b8197e3ae63c1058003fb56f174321e44e50
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2025-05-20 08:55:09 +0900

    first commit

--- pg_practice.sql
+++ pg_practice.sql
@@ -4,20 +4,18 @@ CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
 
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` char(255) NOT NULL DEFAULT '' Comment '氏名',
-  `age` int NOT NULL DEFAULT 0 Comment '年齢',
-  `gender` enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' Comment '性別',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL DEFAULT '', 
+  age INTEGER NOT NULL DEFAULT 0,        
+  gender VARCHAR(10) NOT NULL DEFAULT 'Other' CHECK (gender IN ('Man', 'Woman', 'Other')) 
+);
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
 
-CREATE TABLE `jobs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `name` char(255) NOT NULL DEFAULT '会社員' Comment '仕事名',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY `fk_user_id` (`user_id`) REFERENCES `users`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE jobs (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL DEFAULT '会社員', 
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
